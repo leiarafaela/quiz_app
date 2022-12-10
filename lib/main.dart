3867c0805263ea5eaafa75aed46d4dcbe82a7aa9
rafaela <rafaela20augusto@gmail.com>
2022-12-10 19:43:49 -0300

    Add ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-import '../resultado.dart';
-import '../questionario.dart';
+import 'home_page.dart';
 
-void main() => runApp(const PerguntaApp());
+void main() => runApp(const QuizApp());
 
-class PerguntaApp extends StatefulWidget {
-  const PerguntaApp({super.key});
-
-  @override
-  _PerguntaAppState createState() {
-    return _PerguntaAppState();
-  }
-}
-
-class _PerguntaAppState extends State<PerguntaApp> {
-  var _perguntaSelecionada = 0;
-  var _pontuacaoTotal = 0.0;
-  final _perguntas = const [
-    {
-      'pergunta': 'Qual empresa criou o Flutter?',
-      'respostas': [
-        {'alternativa': 'Microsoft', 'pontuacao': 0},
-        {'alternativa': 'Google', 'pontuacao': 2.5},
-        {'alternativa': 'AWS', 'pontuacao': 0},
-        {'alternativa': 'Apple', 'pontuacao': 0},
-      ]
-    },
-    {
-      'pergunta': 'Em que ano o Flutter foi lançado?',
-      'respostas': [
-        {'alternativa': '2010', 'pontuacao': 0},
-        {'alternativa': '2015', 'pontuacao': 0},
-        {'alternativa': '2017', 'pontuacao': 2.5},
-        {'alternativa': '2019', 'pontuacao': 0},
-      ]
-    },
-    {
-      'pergunta': 'Qual linguagem de programação é utilizado com o Flutter?',
-      'respostas': [
-        {'alternativa': 'JavaScript', 'pontuacao': 0},
-        {'alternativa': 'Python', 'pontuacao': 0},
-        {'alternativa': 'Dart', 'pontuacao': 2.5},
-        {'alternativa': 'C#', 'pontuacao': 0},
-      ]
-    },
-    {
-      'pergunta': 'O Flutter é multiplataforma?',
-      'respostas': [
-        {'alternativa': 'Não, suporta apenas Android', 'pontuacao': 0},
-        {'alternativa': 'Não, suporta apenas iOS', 'pontuacao': 0},
-        {
-          'alternativa': 'Sim, suporta  o Android, iOS entre outros',
-          'pontuacao': 2.5
-        },
-        {'alternativa': 'Não, suporta apenas Web', 'pontuacao': 0},
-      ]
-    },
-  ];
-
-  void _responder(double pontuacao) {
-    if (temPerguntaSelecionada) {
-      setState(() {
-        _perguntaSelecionada++;
-        _pontuacaoTotal += pontuacao;
-      });
-    }
-  }
-
-  void _reiniciarApp() {
-    setState(() {
-      _perguntaSelecionada = 0;
-      _pontuacaoTotal = 0;
-    });
-  }
-
-  bool get temPerguntaSelecionada {
-    return _perguntaSelecionada < _perguntas.length;
-  }
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Perguntas'),
-        ),
-        body: temPerguntaSelecionada
-            ? Questionario(
-                perguntas: _perguntas,
-                perguntaSelecionada: _perguntaSelecionada,
-                responder: _responder)
-            : Resultado(
-                pontuacao: _pontuacaoTotal,
-                reiniciarApp: _reiniciarApp,
+      theme: ThemeData(
+          fontFamily: GoogleFonts.lato().fontFamily,
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: const Color.fromARGB(255, 1, 13, 34),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 1, 13, 34),
+              textStyle: const TextStyle(
+                color: Colors.white,
               ),
-      ),
+            ),
+          ),
+          scaffoldBackgroundColor: const Color(0XFFCED4DA)),
+      home: const HomePage(),
     );
   }
 }
